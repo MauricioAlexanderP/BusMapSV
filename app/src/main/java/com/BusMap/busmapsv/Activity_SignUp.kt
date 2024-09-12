6c61108c3b36bc2201cb4f57b7ecd2a47482405c
Mauricio Perez <mauricio.perez24@itca.edu.sv>
2024-09-12 07:44:14 -0600

    feat: Implement persistent login using SharedPreferences

--- app/src/main/java/com/BusMap/busmapsv/Activity_SignUp.kt
+++ app/src/main/java/com/BusMap/busmapsv/Activity_SignUp.kt
@@ -1,9 +1,7 @@
 package com.BusMap.busmapsv
 
-import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -12,24 +10,13 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import java.util.regex.Pattern
 
-
 class Activity_SignUp : AppCompatActivity() {
-
-    private lateinit var googleSignInClient: GoogleSignInClient
-    private val RC_SIGN_IN = 9001
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -40,65 +27,7 @@ class Activity_SignUp : AppCompatActivity() {
             insets
         }
 
-        // Configura Google Sign-In
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))  // Obtén esto desde Firebase Console
-            .requestEmail()
-            .build()
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
-
-        //Configura el botón de inicio de sesión con Google
-        val googleSignUpButton = findViewById<Button>(R.id.btnGoogleSignUp)
-
-        //al hacer clic en el botón de inicio de sesión con Google, inicia el proceso de inicio de sesión con Google
-        googleSignUpButton.setOnClickListener {
-            signInWithGoogle()
-        }
-
         authUser()
-    }
-    // Iniciar el flujo de inicio de sesión con Google
-    private fun signInWithGoogle() {
-        val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, RC_SIGN_IN)
-    }
-    // Capturar el resultado del inicio de sesión
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == RC_SIGN_IN) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                val account = task.getResult(ApiException::class.java)
-                firebaseAuthWithGoogle(account.idToken!!)
-            } catch (e: ApiException) {
-                Log.w("SignInActivity", "Google sign in failed", e)
-                Toast.makeText(this, "Error en el inicio de sesión con Google", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    // Autenticación en Firebase con el token de Google
-    private fun firebaseAuthWithGoogle(idToken: String) {
-        val credential = GoogleAuthProvider.getCredential(idToken, null)
-        FirebaseAuth.getInstance().signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Inicio de sesión exitoso
-                    val user = FirebaseAuth.getInstance().currentUser
-                    updateUI(user)
-                } else {
-                    Toast.makeText(this, "Error de autenticación con Google", Toast.LENGTH_SHORT).show()
-                }
-            }
-    }
-    // Actualizar la interfaz del usuario después del inicio de sesión
-    private fun updateUI(user: FirebaseUser?) {
-        user?.let {
-            Toast.makeText(this, "Bienvenido ${user.displayName}", Toast.LENGTH_SHORT).show()
-            // Redirigir a la actividad principal
-            goToActivityHome()
-        }
     }
 
     private fun authUser() {
@@ -208,7 +137,6 @@ class Activity_SignUp : AppCompatActivity() {
                                 .document(userId)
                                 .set(userData)
                                 .addOnSuccessListener {
-
                                     // Guardar el estado de inicio de sesión en SharedPreferences
                                     val sharedPreferences = getSharedPreferences("user_prefs", MODE_PRIVATE)
                                     val editor = sharedPreferences.edit()
@@ -217,8 +145,6 @@ class Activity_SignUp : AppCompatActivity() {
 
                                     showVerificationDialog()
                                     //goToActivityHome() // Llamada para ir a la pantalla principal
-
-
                                 }
                                 .addOnFailureListener {
                                     Toast.makeText(this, "Error al guardar la información del usuario", Toast.LENGTH_LONG).show()
