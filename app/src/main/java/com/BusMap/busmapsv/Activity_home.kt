47601fc8abd125bf82f4402398b66055487b1f05
Mauricio Perez <mauricio.perez24@itca.edu.sv>
2024-09-20 19:05:23 -0600

    Iconos de la App y splash Screen

--- app/src/main/java/com/BusMap/busmapsv/Activity_home.kt
+++ app/src/main/java/com/BusMap/busmapsv/Activity_home.kt
@@ -1,5 +1,6 @@
 package com.BusMap.busmapsv
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import android.webkit.WebView
@@ -14,6 +15,7 @@ class Activity_home : AppCompatActivity() {
 
     private lateinit var webView: WebView
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -29,7 +31,7 @@ class Activity_home : AppCompatActivity() {
 
         // Obtener referencia al WebView y al botón
         val webView: WebView = findViewById(R.id.webView)
-        val loadButton: Button = findViewById(R.id.btnCargarMapa)
+        //val loadButton: Button = findViewById(R.id.btnCargarMapa)
 
         // Habilitar JavaScript si es necesario
         webView.settings.javaScriptEnabled = true
@@ -38,6 +40,7 @@ class Activity_home : AppCompatActivity() {
         webView.webViewClient = WebViewClient()
 
         // Asignar el evento de clic al botón
+        val loadButton = findViewById<Button>(R.id.btnCargarMapa)
         loadButton.setOnClickListener {
             // Cargar la URL en el WebView
             webView.loadUrl("https://api.mapbox.com/styles/v1/busmap/cm0zeyfjq00ep01nqfhkwh8go/draft.html?title=view&access_token=pk.eyJ1IjoiYnVzbWFwIiwiYSI6ImNtMHplc3o0eDA1a2QyaXE4cW94c3V6MW4ifQ.pSxWCGCbp0h3_41MUGNiYw&zoomwheel=true&fresh=true#8.58/13.725/-88.8391")
